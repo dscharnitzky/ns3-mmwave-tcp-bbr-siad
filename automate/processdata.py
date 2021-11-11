16bd13f13bbf09e247a9917d7cd218e5d4821d3d
schdwlk <dscharnitzky@hotmail.com>
2021-11-11 16:31:06 +0100

    Fixed inaccurate processdata

--- automate/processdata.py
+++ automate/processdata.py
@@ -6,20 +6,28 @@ if __name__ == "__main__":
     lines = f.readlines()
     sum = 0.0
     nextTime = 0.1
+    startTime = 0.0
+    lastTime = 0.1
     for line in lines:
         time, value = line.split()
         time = float(time)
+        lastTime = time
         value = int(value)
         if time < nextTime:
             sum = sum + value
         else:
-            sum = 10.0 * sum / (1024.0 * 1024.0)
+            if lastTime - startTime < 0.001:
+                sum = 0.0
+            else:
+                sum = sum / (1024.0 * 1024.0 * (lastTime - startTime))
             timeStr = str(time - 0.1)
             sumStr = str(sum)
             outLine = timeStr + "; " + sumStr + "\n"
             w.write(outLine)
             sum = value
             nextTime = nextTime + 0.1
+            startTime = time
 
     f.close()
     w.close()
+
